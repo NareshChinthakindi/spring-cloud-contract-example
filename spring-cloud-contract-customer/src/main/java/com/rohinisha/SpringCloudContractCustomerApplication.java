ece61cc1d283c4093e8fa54eb2644cc2b0983c7d
Naresh_Chinthakindi <naresh.chinthakindi@gmail.com>
2020-05-19 18:33:00 +0530

    Addded new project contract producer

--- spring-cloud-contract-customer/src/main/java/com/rohinisha/SpringCloudContractCustomerApplication.java
+++ spring-cloud-contract-customer/src/main/java/com/rohinisha/SpringCloudContractCustomerApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringCloudContractProducerApplication {
+public class SpringCloudContractCustomerApplication {
 
-	//Test
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudContractProducerApplication.class, args);
+		SpringApplication.run(SpringCloudContractCustomerApplication.class, args);
 	}
 
 }
