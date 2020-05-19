c61e7759406c68ec638ecb835bde9ade4d7e0458
Naresh_Chinthakindi <naresh.chinthakindi@gmail.com>
2020-05-19 18:08:46 +0530

    Addded new project contract producer

---
+++ spring-cloud-contract-producer/src/test/java/com/rohinisha/SpringCloudContractProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohinisha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudContractProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
