52be1326e49f4417a8ad2b52fc6f084cedfa31c3
naresh.chinthakindi@gmail.com <naresh.chinthakindi@gmail.com>
2020-05-21 08:58:36 +0530

    Pushed latest changes

--- spring-cloud-contract-customer/src/main/java/com/rohinisha/SpringCloudContractCustomerApplication.java
+++ spring-cloud-contract-customer/src/main/java/com/rohinisha/SpringCloudContractCustomerApplication.java
@@ -2,10 +2,15 @@ package com.rohinisha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringCloudContractCustomerApplication {
-
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudContractCustomerApplication.class, args);
 	}
